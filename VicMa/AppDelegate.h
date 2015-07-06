35184bfee95172972b7bb004687913a439d6be3d
Sujith Achuthan <Suji@dhcp020-194-229.wireless.buffalo.edu>
2015-07-06 12:02:14 -0400

    Initial Commit

---
+++ VicMa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VicMa
+//
+//  Created by Sujith Achuthan on 7/6/15.
+//  Copyright (c) 2015 Sujith Achuthan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VicMa/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VicMa
+//
+//  Created by Sujith Achuthan on 7/6/15.
+//  Copyright (c) 2015 Sujith Achuthan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
